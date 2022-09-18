f118ad43847c03ebfae7ac06217a1265a002bf05
mateusnagelo <mateusgenius1234@hotmail.com>
2022-09-18 14:31:19 -0300

    Inclusao mask com javascript

--- scripts/validcpf.js
+++ scripts/validcpf.js
@@ -1,6 +1,6 @@
 $("#cpf").mask("000.000.000-00")
 $("#cnpj").mask("00.000.000/0000-00")
-$("#telefone").mask("(00) 0000-0000")
+$("#telefone").mask("(00) 00000-0000")
 $("#salario").mask("999.999.990,00", { reverse: true })
 $("#cep").mask("00.000-000")
 $("#dataNascimento").mask("00/00/0000")

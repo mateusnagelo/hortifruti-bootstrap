2d51839d7056895bb286369fce4fbe365306e678
mateusnagelo <mateusgenius1234@hotmail.com>
2022-09-20 10:12:49 -0300

    Validações dos compos abrigatorios com JS

---
+++ scripts/cliente_dados.js
@@ -0,0 +1,40 @@
+$('#form-contato').submit(function() {
+    var nome = $('#nome');
+    var cpf = $('#cpf');
+    var data = $('#data');
+    var erro = $('.alert');
+    var campo = $('#campo-erro');
+
+    // removendo o elemento da tela sempre que tentar submeter o formulario
+    erro.addClass('d-none');
+    $('.is-invalid').removeClass('is-invalid');
+
+    // valida o campo nome
+    if (nome.val() == '') {
+        erro.removeClass('d-none');
+        campo.html('nome'); // nome do campo que não foi preenchido!
+        nome.focus();
+        nome.addClass('is-invalid');
+        return false;
+    }
+
+    if (cpf.val() == '') {
+        erro.removeClass('d-none');
+        campo.html('cpf'); // nome do campo que não foi preenchido!
+        cpf.focus();
+        cpf.addClass('is-invalid');
+        return false;
+    }
+
+    // valida o campo mensagem
+    if (data.val() == '') {
+        erro.removeClass('d-none');
+        campo.html('data'); // nome do campo que não foi preenchido!
+        data.focus();
+        data.addClass('is-invalid');
+        return false;
+    }
+
+    // se chegar aqui pode enviar os dados!
+    return true;
+});
